308a0307eaa0a35ce651e703f1282216fe54c2ee
bklukaczewski <bklukaczewski@gmail.com>
2019-02-16 14:27:46 +0100

    Show user email in Create content screen.

--- src/screens/CreateContentScreen.tsx
+++ src/screens/CreateContentScreen.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Text } from 'react-native';
 
 import { FullScreenTemplate } from 'components';
+import firebase from 'react-native-firebase';
 
 export class CreateContentScreen extends React.PureComponent {
   render() {
     return (
       <FullScreenTemplate padded>
         <Text>Content create</Text>
+        <Text>Hello {firebase.auth().currentUser!.email}</Text>
       </FullScreenTemplate>
     );
   }
